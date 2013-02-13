cc2a65ef1160e83cdba29cd3067b733348f92e5d
Roman Scherer <roman.scherer@nugg.ad>
2013-02-13 15:36:53 +0100

    Initial commit.

---
+++ vertica-mode.el
@@ -0,0 +1,82 @@
+;;; vertica-mode.el --- Vertica SQL mode extension
+
+;; Copyright (C) 2013 Roman Scherer
+
+;; Author: Roman Scherer <roman@burningswell.com>
+;; Version: 0.1
+;; Package-Requires: ((sql-mode "3.0"))
+;; Keywords: sql vertica
+
+
+;;; Commentary:
+
+;; This package adds Vertica to the sql-mode product list.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;;###autoload
+(require 'sql)
+
+(defvar sql-product-alist-backup sql-product-alist)
+(setq sql-product-alist sql-product-alist-backup)
+
+(defcustom sql-vertica-program "vsql"
+  "Command to start the Vertica client."
+  :type 'file
+  :group 'SQL)
+
+(defcustom sql-vertica-options '()
+  "List of additional options for `sql-vertica-program'."
+  :type '(repeat string)
+  :group 'SQL)
+
+(defcustom sql-vertica-login-params '(user password database server)
+  "List of login parameters needed to connect to Vertica."
+  :type 'sql-login-params
+  :group 'SQL)
+
+(defun sql-comint-vertica (product options)
+  "Create comint buffer and connect to Vertica."
+  (let ((params options))
+    (if (not (string= "" sql-server))
+        (setq params (append (list "-h" sql-server) params)))
+    (if (not (string= "" sql-database))
+        (setq params (append (list "-d" sql-database) params)))
+    (if (not (string= "" sql-password))
+        (setq params (append (list "-w" sql-password) params)))
+    (if (not (string= "" sql-user))
+        (setq params (append (list "-U" sql-user) params)))
+    (sql-comint product params)))
+
+(defun sql-vertica (&optional buffer)
+  "Run vsql as an inferior process."
+  (interactive "P")
+  (sql-product-interactive 'vertica buffer))
+
+(setq sql-product-alist
+      (cons '(vertica
+              :sqli-program sql-vertica-program
+              :sqli-options sql-vertica-options
+              :sqli-login sql-vertica-login-params
+              :sqli-comint-func sql-comint-vertica
+              :prompt-regexp "^\\w*=[#>] "
+              :prompt-length 5
+              :prompt-cont-regexp "^\\w*[-(][#>] ")
+            sql-product-alist))
+
+(provide 'vertica-mode)
